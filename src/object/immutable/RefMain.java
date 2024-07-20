23065825ec1ff28bf82dcb96289851edf2e62d13
Park Hyewon <db1648@naver.com>
2024-07-20 17:32:05 +0900

    240720 불변객체 활용 예제

---
+++ src/object/immutable/RefMain.java
@@ -0,0 +1,17 @@
+package object.immutable;
+
+public class RefMain {
+    public static void main(String[] args) {
+        ImmutableAddress a = new ImmutableAddress("서울");
+        ImmutableAddress b = a; //참조값 대입을 막을 수 있는 방법은 없다.
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+//        b.setValue("부산") //b의 값을 부산으로 변경 불가
+        b = new ImmutableAddress("부산"); //새로운 객체 생성
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+    }
+}
